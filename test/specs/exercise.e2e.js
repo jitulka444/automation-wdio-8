d22b8b70ade3290547ee627d8c45be35fe48da84
jitulka444 <jitulka444@gmail.com>
2023-05-24 10:56:30 +0200

    homework5

--- test/specs/exercise.e2e.js
+++ test/specs/exercise.e2e.js
@@ -10,7 +10,48 @@ describe('Czechitas Login Page', async () => {
 
         await browser.url('/prihlaseni');
 
-        await browser.pause(5000);
+        const loginButton = $ ('.btn-primary');
+        await loginButton.click();
+
+        
+        const emailField = $('#email');
+        await emailField.setValue('da-app.admin@czechitas.cz');
+
+        const passwordField = $ ('#password');
+        await passwordField.setValue('123');
+
+        await loginButton.click();
+
+        const fieldError = $('.invalid-feedback');
+        console.log('Field error: ' + await fieldError.getText());
+
+
+        const emailField = $('#email');
+        await emailField.setValue('da-app.admin@czechitas.cz');
+
+        const passwordField = $ ('#password');
+        await passwordField.setValue('Czechitas123');
+
+        const loginbutton = $ ('.btn-primary');
+        await loginbutton.click();
+
+        const userNameDropdown = $('.navbar-right').$('[data-toggle="dropdown"]');
+        console.log('User currently logged in: ' + await userNameDropdown.getText());
+
+        await $('=Přihlášky').click();
+
+        const rows = await $('.dataTable').$('tbody').$$('tr');
+        console.log('There are ' + rows.length + ' rows in the table');
+        for (const row of rows) {
+            const rowText = await row.getText()
+            console.log(rowText);
+        }
+        
+
+
+
+
+
 
         
 
--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,11 +1,31 @@
-describe('Homework', async () => {
 
-    it('should open page and create screenshot', async () => {
+describe('Registration Page', async () => {
 
-        escribe('Homework', async () => {
+        });
 
-            it('new registration', async () => {
+    it('should show registration form', async () => {
+
+        const nameField = $('#name');
+        console.log('Name field is dislayed: ' + await nameField.isDisplayed());
+        console.log('Name field is dislayed: ' + await nameField.isEnabled());
         
+        const emailField = $('#email');
+        console.log('Email field is dislayed: ' + await emailField.isDisplayed());
+        console.log('Email field is dislayed: ' + await emailField.isEnabled());
+
+        const passwordField = $('#password');
+        console.log('Password field is dislayed: ' + await passwordField.isDisplayed());
+        console.log('Password field is dislayed: ' + await passwordField.isEnabled());
+
+        const submitButton = $('.btn-primary');
+        console.log('Submit button is dislayed: ' + await submitButton.isDisplayed());
+        console.log('Submit button text is: ' + await submitButton.getText());
+
+});
+
+ it('new registration', async () => {
+
+                
                 await browser.reloadSession();
         
                 await browser.url('/registrace');
@@ -26,12 +46,63 @@ describe('Homework', async () => {
                 const submitButton = $ ('.btn-primary');
                 await submitButton.click();
                 
-        
+                const userNameDropdown = $('.navbar-right').$('[data-toggle="dropdown"]');
+        console.log('User currently logged in: ' + await userNameDropdown.getText());
             });
         
-        });
+            it('unsuccessful registration with existing email', async () => {
+
+                
+                await browser.reloadSession();
         
+                await browser.url('/registrace');
+                
+                const nameField = $('#name');
+                await nameField.setValue("Jitka Michálková");
+        
+        
+                const emailField = $ ('#email');
+                await emailField.setValue("da-app.admin@czechitas.cz");
+        
+                const passwordField = $ ('#password');
+                await passwordField.setValue("Heslo1234");
+        
+                const passwordconfirmField = $ ('#password-confirm');
+                await passwordconfirmField.setValue("Heslo1234");
+        
+                const submitButton = $ ('.btn-primary');
+                await submitButton.click();
+                
+                const fieldError = $$('.invalid-feedback');
+        console.log('Field error: ' + await fieldError.getText());
+            });
+        
+            it('unsuccessful registration with invalid password', async () => {
+
+                
+                await browser.reloadSession();
+        
+                await browser.url('/registrace');
+                
+                const nameField = $('#name');
+                await nameField.setValue("Jitka Michálková");
+        
+        
+                const emailField = $ ('#email');
+                await emailField.setValue("michalkova.jitka@email.com");
+        
+                const passwordField = $ ('#password');
+                await passwordField.setValue("1234");
+        
+                const passwordconfirmField = $ ('#password-confirm');
+                await passwordconfirmField.setValue("1234");
+        
+                const submitButton = $ ('.btn-primary');
+                await submitButton.click();
+                
+                const fieldError = $$('.invalid-feedback');
+        console.log('Field error: ' + await fieldError.getText());
 
     });
+        
 
-});
